1e10a20edc3dddc8da3c2447539e66fc23da5140
madkings <unconfigured@null.spigotmc.org>
2023-09-10 17:14:07 +0300

    hw 1 prepared

---
+++ main.js
@@ -0,0 +1,8 @@
+const tempCelsium = 36.6;
+const tempFahrenheit = (9 / 5) * tempCelsium + 32;
+console.log(tempFahrenheit);
+
+let name = "Dmitriy";
+let admin = undefined;
+admin = name;
+console.log(admin);
